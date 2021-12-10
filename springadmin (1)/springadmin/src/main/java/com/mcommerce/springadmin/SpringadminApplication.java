14918a377bf8f3e94be5b7077e962f76d78cbfa6
Dell <abdoo.chbrik@gmail.com>
2021-12-11 00:47:43 +0100

    create spring server

---
+++ springadmin (1)/springadmin/src/main/java/com/mcommerce/springadmin/SpringadminApplication.java
@@ -0,0 +1,13 @@
+package com.mcommerce.springadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringadminApplication.class, args);
+	}
+
+}
